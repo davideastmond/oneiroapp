8f0a668c50d6633d5fae48e3ae02c274fa79dcb8
David Eastmond <yojoedave2002@yahoo.com>
2018-03-07 09:16:45 -0500

    Mar 7 09h16

--- Oneiro/dcListViewController.m
+++ Oneiro/dcListViewController.m
@@ -74,7 +74,7 @@
 }
 -(CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
-    return [indexPath row] * 75;
+    return 75;
+    //return [indexPath row] * 75;
 }
 @end
